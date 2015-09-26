acca728739c1c0fb1d9e3e2cac92d4be8cd78a13
Jeff H <jahuangi@yahoo.ca>
2015-09-26 15:33:51 -0700

    added store model

---
+++ db/migrate/20150926220623_create_stores.rb
@@ -0,0 +1,12 @@
+class CreateStores < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :province
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/stores.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :store do
+    name "MyString"
+address "MyString"
+city "MyString"
+province "MyString"
+  end
+
+end
