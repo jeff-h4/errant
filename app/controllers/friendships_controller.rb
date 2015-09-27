7d0ac6acee26f789ef0cfe81d28574c3ed75731b
Jeff H <jahuangi@yahoo.ca>
2015-09-26 21:57:58 -0700

    added self-referencing many2many Friendship association with User/Friends

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,21 @@
+class FriendshipsController < ApplicationController
+  def create
+    #@friendship = Friendship.new(params[:friendship])
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    if @friendship.save
+      flash[:notice] = "Added Friend"
+      redirect_to root_url
+    else
+      flash[:error] = "Problem adding friend"
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    #@friendship = Friendship.find(params[:id])
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Ditched Friend"
+    redirect_to current_user
+  end
+end
