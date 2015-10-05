33cc84ada9883e7f1ed07e774f3fbf2135244b8a
Jeff H <jahuangi@yahoo.ca>
2015-10-05 16:17:23 -0700

    added seeding to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+userA = FactoryGirl::create(:user,{first_name: "a",
+                             last_name: "a",
+                             email: "a@a.com",
+                             password: "a"})
+userB = FactoryGirl::create(:user,{first_name: "b",
+                             last_name: "b",
+                             email: "b@b.com",
+                             password: "b"})
+userC = FactoryGirl::create(:user,{first_name: "c",
+                             last_name: "c",
+                             email: "c@c.com",
+                             password: "c"})
+10.times do
+  errand = FactoryGirl::build(:errand)
+  errand.owner = userA
+  errand.runner = userB
+  errand.save
+end
+10.times do
+  errand = FactoryGirl::build(:errand)
+  errand.owner = userA
+  errand.runner = userC
+  errand.save
+end
+10.times do
+  errand = FactoryGirl::build(:errand)
+  errand.owner = userB
+  errand.runner = userA
+  errand.save
+end
